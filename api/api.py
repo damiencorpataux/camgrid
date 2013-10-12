517459b12dea9cbc029991ca13b7c5cbc48fcc6f
Damien Corpataux <d@mien.ch>
2013-10-12 04:14:18 +0200

    Added motion.conf parser + on-the-fly preview image extractor

--- api/api.py
+++ api/api.py
@@ -1,27 +1,51 @@
-from bottle import Bottle, run
+from bottle import Bottle, run, response, abort
 from glob import glob
+import os, re, subprocess
 
 app = Bottle()
 
 c = config = {
-    'events': {
-        'path': '/home/damien/motion-storage'
-    }
+    'filespath': '/home/damien/motion-storage',
+    'motionconf': '/etc/motion/motion.conf',
+    'motion': {}
 }
 
 @app.route('/')
-def hello():
-    return "Hello World!"
+@app.route('/config')
+def get_config():
+    return c
 
 @app.route('/events')
 def get_events():
-    path = c['events']['path']+'/*/*.avi'
+    path = c['motion']['target_dir']+'/*/*.avi'
     files = glob(path)
     return {
         'path': path,
         'count': len(files),
         'files': files,
     }
+
+@app.route('/events/meta/<file:path>')
+def get_meta():
+    return {}
+
+@app.route('/preview/<file:path>')
+@app.route('/preview/<file:path>/<time:int>')
+def get_preview(file, time=0):
+    file = '/' + file
+    if (not os.path.isfile(file)): abort(404, 'File %s does not exist' % file)
+    cmd = 'ffmpeg -i "%s" -vframes 1 -an -f image2 -s 640x480 -ss %s - 2>/dev/null' % (file, time)
+    response.content_type = 'image/jpeg'
+    return subprocess.check_output(cmd, shell=True)
+
+def parse_motion_conf():
+    match = re.compile("^(target_dir|movie_filename|jpeg_filename) (.*)$").match
+    with open(c['motionconf'], 'r') as file:
+        conf = {m.group(1):m.group(2) for m in [match(l) for l in file] if m}
+    c['motion'] = conf
+
+
+parse_motion_conf()
 
 # Server instance
 run(app, host='0.0.0.0', port=8000, reloader=True)
